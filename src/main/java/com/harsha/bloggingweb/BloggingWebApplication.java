4f6866d1b70716113cc0d7eb67285b14b68a7ab0
Harsha Daryani <harsha.daryani@clairvoyantsoft.com>
2019-06-25 16:34:00 +0530

    Initializing spring boot app

---
+++ src/main/java/com/harsha/bloggingweb/BloggingWebApplication.java
@@ -0,0 +1,13 @@
+package com.harsha.bloggingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingWebApplication.class, args);
+	}
+
+}
